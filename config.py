71ec5de68ff8f8abf87a798222d7bfc267b83f7c
RomikIzBRU <dysenkov.roma@yandex.ru>
2025-05-18 16:16:07 +0300

    Версия с FAQ и Связью с сотрудниками

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # Загружаем переменные окружения из .env
+load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
 SECONDARY_BOT_TOKEN = os.getenv("SECONDARY_BOT_TOKEN")
-ADMIN_USERS = os.getenv("ADMIN_USERS").split(",")  # если будет несколько через запятую
+ADMIN_USERS = os.getenv("ADMIN_USERS")
 ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
 RESPONSABLE_EMPLOYER = os.getenv("RESPONSABLE_EMPLOYER")
